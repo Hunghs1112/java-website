26eee73f0d9a76ddaf517e0ef82eb2fc02ee4e26
Hunghs1112 <157335285+Hunghs1112@users.noreply.github.com>
2024-06-01 23:03:51 +0700

    create file

---
+++ src/main/java/com/example/new_website/NewWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.new_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewWebsiteApplication.class, args);
+	}
+
+}
