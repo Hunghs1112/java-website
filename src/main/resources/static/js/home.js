bbd2f1f22e9b1150380bb5d1000afa67cee11945
Hunghs1112 <157335285+Hunghs1112@users.noreply.github.com>
2024-06-04 02:39:53 +0700

    thêm fe cho trang chính

---
+++ src/main/resources/static/js/home.js
@@ -0,0 +1,39 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 20000); // Change image every 20 seconds
+}
+
+function plusSlides(n) {
+    slideIndex += n;
+    if (slideIndex > document.getElementsByClassName("mySlides").length) {slideIndex = 1}
+    if (slideIndex < 1) {slideIndex = document.getElementsByClassName("mySlides").length}
+    showCurrentSlide();
+}
+
+function showCurrentSlide() {
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
