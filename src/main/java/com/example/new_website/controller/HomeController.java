6ecbf720e906644d6bdf70db6b026ba8fe3bf2ca
Hunghs1112 <157335285+Hunghs1112@users.noreply.github.com>
2024-06-04 15:00:58 +0700

    xây dựng trang chính

--- src/main/java/com/example/new_website/controller/HomeController.java
+++ src/main/java/com/example/new_website/controller/HomeController.java
@@ -2,12 +2,60 @@ package com.example.new_website.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
-    public String home() {
+    @RequestMapping("/")
+    public String index() {
+        return "redirect:/home";
+    }
+
+    @RequestMapping("/home")
+    public String homePage() {
         return "home";
+    }
+
+    @GetMapping("/about")
+    public String aboutPage() {
+        return "about";
+    }
+
+
+
+    @GetMapping("/blog")
+    public String blogPage() {
+        return "blog";
+    }
+
+    @GetMapping("/software")
+    public String softwarePage() {
+        return "software";
+    }
+
+    @GetMapping("/training")
+    public String trainingPage() {
+        return "training";
+    }
+
+    @GetMapping("/jobs")
+    public String jobsPage() {
+        return "jobs";
+    }
+
+    @GetMapping("/materials")
+    public String materialsPage() {
+        return "materials";
+    }
+
+    @GetMapping("/faq")
+    public String faqPage() {
+        return "faq";
+    }
+
+    @GetMapping("/register")
+    public String registerPage() {
+        return "register";
     }
 }
