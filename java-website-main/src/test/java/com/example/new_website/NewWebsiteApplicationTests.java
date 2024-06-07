c2560621991ee68c4c6972354ad99520689e474c
UinTus <tuuyen294@gmail.com>
2024-06-07 17:07:57 +0700

    add training.html

---
+++ java-website-main/src/test/java/com/example/new_website/NewWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.new_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
